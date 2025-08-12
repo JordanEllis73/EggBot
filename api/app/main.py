8b591215c95bbb7fc858f7eecc1fd1b4c9373068
Jordan Ellis <jellis73@gatech.edu>
2025-08-11 23:57:42 -0400

    dump typo

--- api/app/main.py
+++ api/app/main.py
@@ -14,7 +14,7 @@ if origins:
     print(f"Origins: {origins}")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[*],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
